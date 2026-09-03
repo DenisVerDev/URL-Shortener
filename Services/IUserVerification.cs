1d1d2f12ae283d20d71eecac3497bddd8bd58b59
DenisVerDev <denisverepadev@gmail.com>
2026-09-03 15:13:26 +0300

    created IUserVerification and installed BCrypt

---
+++ Services/IUserVerification.cs
@@ -0,0 +1,29 @@
+﻿using URL_Shortener.Data.Models;
+
+namespace URL_Shortener.Services
+{
+    public interface IUserVerification
+    {
+        Task<UserVerificationResult> VerifyUserAsync(string login, string password);
+    }
+
+    public class UserVerificationResult
+    {
+        public User? User { get; private set; }
+
+        public UserVerificationResultCode Status { get; private set; }
+
+        public UserVerificationResult(User? user, UserVerificationResultCode status)
+        {
+            User = user;
+            Status = status;
+        }
+    }
+
+    public enum UserVerificationResultCode
+    {
+        Success,
+        AbsentUser,
+        VerificationFailure
+    }
+}
