295f311c6633e2addc3621960b111926568782f8
DenisVerDev <denisverepadev@gmail.com>
2026-09-03 17:10:57 +0300

    created foundation for future tests for IURLsManagementService

---
+++ URL-Shortener.Tests/ServicesTests/URLsManagementServiceTests/UMSDeleteAllTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace URL_Shortener.Tests.ServicesTests.URLsManagementServiceTests
+{
+    public class UMSDeleteAllTests
+    {
+    }
+}
