92a0a965da8aa6822c967d21b28ff05f4580d67c
DenisVerDev <denisverepadev@gmail.com>
2026-09-03 13:28:24 +0300

    created IUsersRepository and it's implementation UsersRepository

---
+++ Data/Repositories/IUsersRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq.Expressions;
+using URL_Shortener.Data.Models;
+
+namespace URL_Shortener.Data.Repositories
+{
+    public interface IUsersRepository
+    {
+        Task<User?> FindUserAsync(int id);
+
+        Task<User?> FindUserAsync(string login);
+
+        Task<User?> FindUserAsync(Expression<Func<User, bool>> predicate);
+
+        Task<List<User>> FindUsersAsync(int roleId);
+
+        Task<List<User>> FindUsersAsync(DateTime registraionDate);
+
+        Task<List<User>> FindUsersAsync(Expression<Func<User, bool>> predicate);
+
+        Task<User?> FirstUserAsync();
+
+        Task<User?> LastUserAsync();
+
+        Task<User?> LastUserAsync(Expression<Func<User, bool>> predicate);
+
+        Task<bool> AnyUserAsync(Expression<Func<User, bool>> predicate);
+    }
+}
