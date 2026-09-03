f4bd82afd75938c5b1ab143191d202787f02068b
DenisVerDev <denisverepadev@gmail.com>
2026-09-03 13:50:18 +0300

    created IRolesRepository and it's implementation RolesRepository

---
+++ Data/Repositories/IRolesRepository.cs
@@ -0,0 +1,11 @@
+﻿using URL_Shortener.Data.Models;
+
+namespace URL_Shortener.Data.Repositories
+{
+    public interface IRolesRepository
+    {
+        Task<Role?> FindRoleAsync(int id);
+
+        Task<List<Role>> GetRolesAsync();
+    }
+}
---
+++ Data/Repositories/RolesRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using URL_Shortener.Data.Models;
+
+namespace URL_Shortener.Data.Repositories
+{
+    public class RolesRepository (UShortDbContext _dbContext) : IRolesRepository
+    {
+        public virtual async Task<Role?> FindRoleAsync(int id)
+            => await _dbContext.Roles.FindAsync(id);
+
+        public virtual async Task<List<Role>> GetRolesAsync()
+            => await _dbContext.Roles.ToListAsync();
+    }
+}
