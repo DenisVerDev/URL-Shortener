02b9d98c0ab551802037f5ffd9cb2addb4abc9bc
DenisVerDev <denisverepadev@gmail.com>
2026-09-03 18:04:44 +0300

    created IUserSessionService and it's implementation

---
+++ URL-Shortener/Services/IUserSessionService.cs
@@ -0,0 +1,11 @@
+﻿using URL_Shortener.Data.Models;
+
+namespace URL_Shortener.Services
+{
+    public interface IUserSessionService
+    {
+        Task CreateCookieSessionAsync(User user);
+
+        Task DeleteCookieSessionAsync();
+    }
+}
---
+++ URL-Shortener/Services/UserSessionService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+using URL_Shortener.Data.Models;
+
+namespace URL_Shortener.Services
+{
+    public class UserSessionService (IHttpContextAccessor _hca) : IUserSessionService
+    {
+        public async Task CreateCookieSessionAsync(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Login),
+                new Claim(ClaimTypes.Role, user.RoleId.ToString())
+            };
+
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var principal = new ClaimsPrincipal(identity);
+
+            await _hca.HttpContext!.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+        }
+
+        public async Task DeleteCookieSessionAsync()
+            => await _hca.HttpContext!.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    }
+}
