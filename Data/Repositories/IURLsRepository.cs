62164e71b0a1e6e37242d24e5fe2aad2617d317e
DenisVerDev <denisverepadev@gmail.com>
2026-09-03 13:05:24 +0300

    created IURLsRepository and it's realization URLsRepository

---
+++ Data/Repositories/IURLsRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq.Expressions;
+using URL_Shortener.Data.Models;
+
+namespace URL_Shortener.Data.Repositories
+{
+    public interface IURLsRepository
+    {
+        Task<URL> AddURLAsync(URL url);
+
+        Task<URL> CreateURLAsync(string originalUrl, User creator);
+
+        Task<URL?> FindURLAsync(int id);
+
+        Task<URL?> FindURLAsync(Expression<Func<URL, bool>> predicate);
+
+        Task<List<URL>> FindURLsAsync(int creatorId);
+
+        Task<List<URL>> FindURLsAsync(Expression<Func<URL, bool>> predicate);
+
+        Task<URL?> FirstURLAsync();
+
+        Task<URL?> LastURLAsync();
+
+        Task<URL?> LastURLAsync(int creatorId);
+
+        Task<URL?> LastURLAsync(Expression<Func<URL, bool>> predicate);
+
+        Task DeleteURLAsync(URL url);
+
+        Task DeleteURLsAsync(IEnumerable<URL> urls);
+    }
+}
---
+++ Data/Repositories/URLsRepository.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq.Expressions;
+using URL_Shortener.Data.Models;
+
+namespace URL_Shortener.Data.Repositories
+{
+    public class URLsRepository (UShortDbContext _dbContext) : IURLsRepository
+    {
+        public virtual async Task<URL> AddURLAsync(URL url)
+        {
+            _dbContext.URLs.Add(url);
+            await _dbContext.SaveChangesAsync();
+
+            return url;
+        }
+
+        public virtual async Task<URL> CreateURLAsync(string originalUrl, User creator) // I hope EF automatically fills URLs nav prop in User
+        {
+            var url = new URL
+            {
+                OriginalURL = originalUrl,
+                ShortURLId = Guid.NewGuid().ToString(),
+                Creator = creator
+            };
+
+            return await AddURLAsync(url);
+        }
+
+        public virtual async Task<URL?> FindURLAsync(int id)
+            => await _dbContext.URLs.FindAsync(id);
+
+        public virtual async Task<URL?> FindURLAsync(Expression<Func<URL, bool>> predicate)
+            => await _dbContext.URLs.FirstOrDefaultAsync(predicate);
+
+        public virtual async Task<List<URL>> FindURLsAsync(int creatorId)
+            => await _dbContext.URLs.Where(u => u.Creator.Id == creatorId).ToListAsync();
+
+        public virtual async Task<List<URL>> FindURLsAsync(Expression<Func<URL, bool>> predicate)
+            => await _dbContext.URLs.Where(predicate).ToListAsync();
+
+        public virtual async Task<URL?> FirstURLAsync()
+            => await _dbContext.URLs.FirstOrDefaultAsync();
+
+        public virtual async Task<URL?> LastURLAsync()
+            => await _dbContext.URLs.LastOrDefaultAsync();
+
+        public virtual async Task<URL?> LastURLAsync(int creatorId)
+            => await _dbContext.URLs.Where(u=>u.Creator.Id == creatorId).LastOrDefaultAsync();
+
+        public virtual async Task<URL?> LastURLAsync(Expression<Func<URL, bool>> predicate)
+            => await _dbContext.URLs.Where(predicate).LastOrDefaultAsync();
+
+        public virtual async Task DeleteURLAsync(URL url)
+        {
+            _dbContext.URLs.Remove(url);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public virtual async Task DeleteURLsAsync(IEnumerable<URL> urls)
+        {
+            _dbContext.URLs.RemoveRange(urls);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
