783b7955cf7ddae5a774a4bb8d735dd5cc8f1404
DenisVerDev <denisverepadev@gmail.com>
2026-09-03 17:36:44 +0300

    created IURLsViewingService and it's implementation

---
+++ URL-Shortener/Services/IURLsViewingService.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq.Expressions;
+using URL_Shortener.Data.Models;
+
+namespace URL_Shortener.Services
+{
+    public interface IURLsViewingService
+    {
+        Task<URL?> ViewURLAsync(int id);
+
+        Task<URL?> ViewURLAsync(string shortUrlId);
+
+        Task<URLsViewingResult> ViewURLsAsync(int creatorId);
+
+        Task<List<URL>> ViewURLsAsync(Expression<Func<URL, bool>> predicate); // I will think about pagination later
+    }
+
+    public class URLsViewingResult
+    {
+        public List<URL>? URLs { get; private set; }
+
+        public URLsOperationResultCode Status { get; private set; }
+
+        public URLsViewingResult(List<URL>? urls, URLsOperationResultCode status)
+        {
+            URLs = urls;
+            Status = status;
+        }
+    }
+}
---
+++ URL-Shortener/Services/URLsViewingService.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq.Expressions;
+using URL_Shortener.Data.Models;
+using URL_Shortener.Data.Repositories;
+
+namespace URL_Shortener.Services
+{
+    public class URLsViewingService(IUsersRepository _ur, IURLsRepository _urlsR) : IURLsViewingService
+    {
+        public virtual async Task<URL?> ViewURLAsync(int id)
+            => await _urlsR.FindURLAsync(id);
+
+        public virtual async Task<URL?> ViewURLAsync(string shortUrlId)
+            => await _urlsR.FirstURLAsync(u => u.ShortURLId == shortUrlId);
+
+        public virtual async Task<URLsViewingResult> ViewURLsAsync(int creatorId)
+        {
+            if(!await _ur.AnyUserAsync(u=>u.Id == creatorId))
+                return new URLsViewingResult(null, URLsOperationResultCode.AbsentUser);
+
+            var urls = await _urlsR.FindURLsAsync(creatorId);
+
+            return new URLsViewingResult(urls, URLsOperationResultCode.Success);
+        }
+
+        public virtual async Task<List<URL>> ViewURLsAsync(Expression<Func<URL, bool>> predicate)
+            => await _urlsR.FindURLsAsync(predicate);
+    }
+}
