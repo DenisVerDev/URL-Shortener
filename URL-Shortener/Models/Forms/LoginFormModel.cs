135aee30ab881cca0470920e791cc286f779fefd
DenisVerDev <denisverepadev@gmail.com>
2026-09-04 10:37:31 +0300

    made ugly Login and Register views

--- URL-Shortener/Models/Forms/LoginFormModel.cs
+++ URL-Shortener/Models/Forms/LoginFormModel.cs
@@ -6,13 +6,13 @@ namespace URL_Shortener.Models.Forms
     {
         [Required(ErrorMessage = "Login is required to sign in into your account.")]
         [StringLength(20, MinimumLength = 6, ErrorMessage = "Login must have at least 6 characters and can be up to 20.")]
-        [RegularExpression(@"^[A-Za-z0-9]+$", ErrorMessage = "Login can contain only English letters and digits.")]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z0-9]+$", ErrorMessage = "Login must contain both only English letters and digits.")]
 
-        public string Login { get; set; } = null!;
+        public virtual string Login { get; set; } = null!;
 
         [Required(ErrorMessage = "Password is required to sign in into your account.")]
         [StringLength(20, MinimumLength = 10, ErrorMessage = "Password must have at least 10 characters and can be up to 20.")]
         [RegularExpression(@"^[A-Za-z0-9]+$", ErrorMessage = "Password can contain only English letters and digits.")]
-        public string Password { get; set; } = null!;
+        public virtual string Password { get; set; } = null!;
     }
 }
--- URL-Shortener/Models/Forms/RegisterFormModel.cs
+++ URL-Shortener/Models/Forms/RegisterFormModel.cs
@@ -4,6 +4,12 @@ namespace URL_Shortener.Models.Forms
 {
     public class RegisterFormModel : LoginFormModel
     {
+        [Required(ErrorMessage = "Password is required to create your new account.")]
+        [StringLength(20, MinimumLength = 10, ErrorMessage = "Password must have at least 10 characters and can be up to 20.")]
+        [RegularExpression(@"^[A-Za-z0-9]+$", ErrorMessage = "Password can contain only English letters and digits.")]
+        public override string Password { get; set; } = null!;
+
+        [Required(ErrorMessage = "You must confirm your password before creating new account.")]
         [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
         public string PasswordConfirmation { get; set; } = null!;
     }
