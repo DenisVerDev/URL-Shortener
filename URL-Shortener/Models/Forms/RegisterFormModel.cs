f924b8f57fd39d55e8b90af36558acf02db672bf
DenisVerDev <denisverepadev@gmail.com>
2026-09-04 09:41:22 +0300

    created form models for LoginView and RegisterView

---
+++ URL-Shortener/Models/Forms/RegisterFormModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace URL_Shortener.Models.Forms
+{
+    public class RegisterFormModel : LoginFormModel
+    {
+        [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
+        public string PasswordConfirmation { get; set; } = null!;
+    }
+}
